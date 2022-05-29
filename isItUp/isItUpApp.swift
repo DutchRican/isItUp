9ff71ea9bfbb916bb39a99760abaf1281639aab8
Paul van Woensel <pvanwoensel@gmail.com>
2022-05-29 12:18:21 -0500

    Initial Commit

---
+++ isItUp/isItUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  isItUpApp.swift
+//  isItUp
+//
+//  Created by Paul van Woensel on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct isItUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
